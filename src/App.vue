c4ac187d6fe08a9a1adbf15427cc34374ead896a
weibang_liang <>
2022-01-04 15:27:46 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <global-header :user="currentUser" />
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">@ 2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'viking',
+  id: 123
+}
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    GlobalHeader
+    // ValidateInput,
+    // ValidateForm
+  },
+  setup() {
+    return {
+      currentUser
+    }
+  }
+})
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Signup
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'signup',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style>
+
+</style>
